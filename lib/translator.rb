1d0309f094f72867be91863fbfb2923369aca181
Sean Gallivan <seanpgallivan@hotmail.com>
2019-11-15 02:52:36 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,7 +4,8 @@ def load_library(path)
   new = {'get_meaning' => {}, 'get_emoticon' => {}}
   emot = YAML.load_file(path)
   emot.each do |k, v|
-    new['get_meaning'][]
+    new['get_meaning'][v[1]] = k
+    new['get_emoticon'][v]
   emot
 end
 pp load_library('./lib/emoticons.yml')
